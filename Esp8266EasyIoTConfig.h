38cd1bf595a611fd862b7520c4c76969b63afe5b
LA7LKA <oehaarberg@gmail.com>
2016-05-23 11:56:36 +0200

    Update Esp8266EasyIoTConfig.h

--- Esp8266EasyIoTConfig.h
+++ Esp8266EasyIoTConfig.h
@@ -5,7 +5,7 @@
 #define AP_PASSWORD	    "xxxxxxxxxxxxxxx"   //ACCES POINT password
 //#define SERVER_IP		"192.168.1.8"     //EasyIoT server IP address
 #define SERVER_IP		"10.0.0.32"     //EasyIoT server IP address
-#define SERVER_PORT		"37602"			  //EasyIoT server port
+#define SERVER_PORT		37602			  //EasyIoT server port
 
 
 #define DEBUG
